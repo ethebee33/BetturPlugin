7dc8a13dd27c89f933bc2fc0142dab957231a085
ethebee33 <ethebeeg3ed@gmail.com>
2024-11-14 14:57:17 -0500

    fix null error in hasEnchant function when a item doesnt have an enchant

--- src/main/java/ethebee3/BetturPlugin/utils/itemUtils.java
+++ src/main/java/ethebee3/BetturPlugin/utils/itemUtils.java
@@ -9,7 +9,11 @@ import java.util.Map;
 public class itemUtils {
     public static int hasEnchant(ItemStack item, Enchantment enchant) {
         int enchant2 = -1;
-        enchant2 = item.getEnchantments().get(enchant);
+        try {
+            enchant2 = item.getEnchantments().get(enchant);
+        } catch (NullPointerException e) {
+            return 0;
+        }
         if (enchant2 == -1) {return enchant2;}
         return 0;
     }
